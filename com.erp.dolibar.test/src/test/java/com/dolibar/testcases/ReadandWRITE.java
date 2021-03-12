8c4aa6259868838e0349c8ebd6ee5246feb09b65
lokesh <lokesh@DESKTOP-P2RM02V>
2021-03-12 17:52:32 +0530

    new

---
+++ com.erp.dolibar.test/src/test/java/com/dolibar/testcases/ReadandWRITE.java
@@ -0,0 +1,21 @@
+package com.dolibar.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ReadandWRITE {
+	
+	
+public	WebDriver driver;
+	@Test(groups="regression")
+
+	public void tester()
+	{
+		
+		
+		System.setProperty("webdriver.chrome.driver", "");
+		System.out.println("heool i am no in ");
+	}
+
+}
---
+++ com.erp.dolibar.test/src/test/java/com/dolibar/testcases/WriteAndread.java
@@ -0,0 +1,12 @@
+package com.dolibar.testcases;
+
+import org.testng.annotations.Test;
+
+public class WriteAndread {
+	@Test(groups ="smoke")
+	public void tester2()
+	{
+		System.out.println("its a smokw testing");
+	}
+
+}
